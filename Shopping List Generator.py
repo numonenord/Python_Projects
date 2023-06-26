480b3fb7465d6c1e42bcc6f1507f6c9d212155a9
Mads Feiring <137306423+numonenord@users.noreply.github.com>
2023-06-26 15:03:53 +0200

    Generates Shopping List Based on All Your Recipes

---
+++ Shopping List Generator.py
@@ -0,0 +1,53 @@
+"""
+SHOPPING LIST GENERATOR
+
+Whenever i mealprep, i deal with a lot of recepies that overlap in terms of their ingridients.
+This simple program takes all your recepies and combines them into a single shopping list.
+"""
+
+import re
+from collections import defaultdict
+
+import re
+from collections import defaultdict
+
+class ShoppingListGenerator:
+    def __init__(self):
+        self.shopping_list = defaultdict(int)
+
+    def parse_input(self, user_input):
+        items = [item.strip() for item in re.split(",|\n", user_input)]
+        for item in items:
+            match = re.match(r'(\w+)\s*(\d*)', item)
+            if match:
+                ingredient, quantity = match.groups()
+                quantity = int(quantity) if quantity else 1
+                yield ingredient.lower(), quantity
+
+    def add_recipe(self):
+        while True:
+            user_input = input("> ")
+            if 'done' in user_input.lower():
+                for ingredient, quantity in self.parse_input(user_input):
+                    self.shopping_list[ingredient] += quantity
+                break
+            else:
+                for ingredient, quantity in self.parse_input(user_input):
+                    self.shopping_list[ingredient] += quantity
+
+    def generate_shopping_list(self):
+        print("Enter recipes. Type 'done' when you've entered all ingredients for a recipe.")
+        print("Format:\n'item quantity, item 2 quantity 2, done' \n'item quantity' <return> 'item 2 quantity 2' <return> ... 'done'")
+        while True:
+            self.add_recipe()
+            if input("Would you like to add another recipe? (y/n)").lower() != 'y':
+                break
+
+        print("\nShopping List:")
+        for item, quantity in self.shopping_list.items():
+            if item != "done":
+                print(f"{item}: {quantity}")
+
+if __name__ == "__main__":
+    s = ShoppingListGenerator()
+    s.generate_shopping_list()
